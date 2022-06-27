d96a6d0135fe0afe2ac9abf33dcf9b0b64210621
Nayahto <nayahto.147@gmail.com>
2022-06-26 23:02:47 -0300

    new commite

--- src/Controllers/ElControllers.js
+++ src/Controllers/ElControllers.js
@@ -15,17 +15,21 @@ const getById = (req, res) => {
     const idServ = Elserv.servById(id1);
     res.send(idServ);
 };
+/*create/criar*/
 const create = (req, res) => {
     const palebody = req.body;
     const newPale = Elserv.servNew(palebody);
     res.send(newPale);
 };
+
+/*atualizar/update*/
 const update = (req, res) => {
     const id2 = req.params.id;
     const param = req.body;
     const updatePale = Elserv.servUp(id2, param);
     res.send(updatePale);
 };
+/*apagar/delete*/
 const apagar = (req, res) => {
     const id3 = req.params.id;
     Elserv.servDel(id3);
